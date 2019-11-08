1c9d76a83aa9a98fb9351ae4eb479dc30a9ad482
DianaContrerasHdz <54340198+DianaContrerasHdz@users.noreply.github.com>
2019-11-07 20:35:41 -0600

    Add files via upload

---
+++ Untitled.m
@@ -0,0 +1,27 @@
+close all
+clc
+
+ promt = 'introduzca el valor (grados) de la primera articulacion:';
+ theta1_deg = input(promt);
+ theta1_Rad = deg2rad(theta1_deg);
+for theta1_Rad=0:0.01:theta1_Rad
+    
+   clf
+PrintAxis();
+p1=[0;0;0];
+L1=8;
+L2=4;
+
+% theta1_deg = 30;
+% theta1_Rad = deg2rad(theta1_deg);
+% theta2_deg = -15;
+% theta2_Rad = deg2rad(theta2_deg);
+
+
+TRz1 = [cos(theta1_Rad) -sin(theta1_Rad) 0 0;sin(theta1_Rad) cos(theta1_Rad) 0 0;0 0 1 0;0 0 0 1];
+TRx1 = [1 0 0 L1;0 1 0 0; 0 0 1 0; 0 0 0 1];
+T1=TRz1*TRx1;
+p2=T1(1:3,4);
+line([p1(1) p2(1)],[p1(2) p2(2)],[p1(2) p2(2)],'color',[0 0 0],'linewidth',5)
+pause(0.1)
+end
